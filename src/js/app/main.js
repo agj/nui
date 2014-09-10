d001e6f44dc30e7be6ccf3e24b9ae3981e4f5d91
agj <agj_chile@hotmail.com>
2014-09-10 12:32:45 -0300

    More light refactoring.

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -4,21 +4,24 @@ define( function(require) {
 
 	require('app/addPassTo')();
 
-	var $ = require('jquery');
+	var $         = require('jquery');
 	var parsePath = require('parse-svg-path');
-	var lazy = require('lazy');
-	var λ = require('app/lambda');
-	var rsvp = require('rsvp');
-	var path = require('path');
+	var lazy      = require('lazy');
+	var λ         = require('app/lambda');
+	var rsvp      = require('rsvp');
+	var path      = require('path');
 
-	var toArray = require('agj/utils/toArray');
-	var log = require('agj/utils/log');
-	var partial = require('agj/function/partial');
+	var toArray  = require('agj/utils/toArray');
+	var log      = require('agj/utils/log');
+	var partial  = require('agj/function/partial');
 	var passThis = require('agj/function/passThis');
-	var hex = require('agj/number/inBase')(16);
+	var seq      = require('agj/function/sequence');
+	var hex      = require('agj/number/inBase')(16);
+	var to       = require('agj/to');
 
-	var when = require('app/when');
-	var doDraw = require('app/doDraw');
+	var when                 = require('app/when');
+	var doDraw               = require('app/doDraw');
+	var map                  = require('app/function/map');
 	var parsePathInstruction = require('app/stroke/parsePathInstruction');
 
 	rsvp.on('error', raise);
@@ -31,14 +34,17 @@ define( function(require) {
 		var kanji = path.params.id;
 		when($.get('./data/svg/' + hex(kanji.charCodeAt(0), 5) + '.svg'))
 		.then( function (svg) {
-			return lazy(toArray($(svg).find('path')))
-				.map( function (path) {
-					return parsePath($(path).attr('d')).map(parsePathInstruction);
-				})
+			return $(svg).find('path')
+				.passTo(toArray)
+				.passTo(lazy)
+				.map(seq($, to.call('attr', ['d'])))
+				.map(parsePath)
+				.map(map(parsePathInstruction))
 				.toArray();
 		})
 		.then(doDraw($('#game')[0]));
 	}));
+
 
 	function raise(err) {
 		console.assert(false, err);
